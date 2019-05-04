7c1fb09db3fad3705c2549d99e5a18e066c34ff2
mpyw <ryosuke_i_628@yahoo.co.jp>
2019-05-05 05:01:49 +0900

    Wording

--- config/laravel-local-class-scope.php
+++ config/laravel-local-class-scope.php
@@ -6,7 +6,7 @@ return [
     | Laravel Local Class Scope - Laravel IDE Helper integration
     |-------------------------------------------------------------
     |
-    | Model::query()->scoped() can be complemented by duplicated Eloquent Builder class definition.
+    | Model::query()->scoped() can be completed by duplicated Eloquent Builder class definition.
     | However, Model::scoped() cannot because it is untraceable from extended class.
     | So we need to rewrite \Eloquent section on _ide_helper.php.
     */
