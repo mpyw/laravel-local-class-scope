baacd10d9bb30115ade7c04d79780cd264e5dc0c
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-11-18 14:38:53 +0900

    Wrap stubs with if (false) { }

--- _laravel_ide_helper.php
+++ _laravel_ide_helper.php
@@ -2,21 +2,23 @@
 
 namespace Illuminate\Database\Eloquent
 {
-    class Builder
-    {
-        /**
-         * Apply Scope to Eloquent\Builder.
-         *
-         * @param  \Illuminate\Database\Eloquent\Scope|string         $scope
-         * @param  mixed                                              ...$parameters
-         * @return $this|\Illuminate\Database\Eloquent\Builder|static
-         */
-        public function scoped($scope, ...$parameters)
+    if (false) {
+        class Builder
         {
-            return (new class() extends Model {
-            })
-                ->newQuery()
-                ->scoped($scope, ...$parameters);
+            /**
+             * Apply Scope to Eloquent\Builder.
+             *
+             * @param  \Illuminate\Database\Eloquent\Scope|string         $scope
+             * @param  mixed                                              ...$parameters
+             * @return $this|\Illuminate\Database\Eloquent\Builder|static
+             */
+            public function scoped($scope, ...$parameters)
+            {
+                return (new class() extends Model {
+                })
+                    ->newQuery()
+                    ->scoped($scope, ...$parameters);
+            }
         }
     }
 }
